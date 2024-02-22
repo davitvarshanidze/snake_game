82d3885ffbe60abe9ff330f92e9b0de411694f2e
Dato <davittvarshanidze@gmail.com>
2024-02-22 15:41:54 +0400

    2

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 import { update as update_snake, draw as draw_snake, snake_speed } from "./snake.js"
 
 let last_render_time = 0;
+const game_board = document.getElementById('game_board')
 
 function main(current_time) {
     window.requestAnimationFrame(main)
     const seconds_since_last_render = (current_time - last_render_time) / 1000
     if (seconds_since_last_render < 1 / snake_speed) return
     last_render_time = current_time
+    update()
+    draw()
 }
 
 window.requestAnimationFrame(main)
@@ -16,5 +19,5 @@ function update() {
 }
 
 function draw() {
-    draw_snake()
+    draw_snake(game_board)
 }
--- snake.js
+++ snake.js
@@ -1,9 +1,26 @@
 export const snake_speed = 2;
+const snake_body = [
+    { x: 10, y: 11 },
+    { x: 11, y: 11 },
+    { x: 12, y: 11 }
+]
 
 export function update() {
-    console.log('update snake')
+    for (let i = snake_body.length - 2; i >= 0; i--) {
+        snake_body[i + 1] = {  ...snake_body[i] }
+    }
+    
+    snake_body[0].x += 1
+    snake_body[0].y += -
+
 }
 
-export function draw() {
-    console.log('draw snake')
+export function draw(game_board) {
+    snake_body.forEach(segment => {
+        const snake_element = document.createElement('div')
+        snake_element.style.gridRowStart = segment.x
+        snake_element.style.gridColumnStart = segment.y
+        snake_element.classList.add('snake')
+        game_board.appendChild(snake_element)
+    })
 }
